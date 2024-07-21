bb85566d4ed2d38b88a49cfcce8fc7eb1dd7309f
Mahmoud Ouf <mahmoudouf504@gmail.com>
2024-07-21 16:18:45 +0300

    edit audio book page

--- lib/audio_book_page/audi_book.dart
+++ lib/audio_book_page/audi_book.dart
@@ -1,15 +1,23 @@
-import 'package:exam_str_c11/audio_book_page/bottom_nav_bar.dart';
-import 'package:exam_str_c11/audio_book_page/home.dart';
-
+import 'package:exam_str_c11/audio_book_page/pages/home.dart';
+import 'package:exam_str_c11/audio_book_page/pages/libaray.dart';
+import 'package:exam_str_c11/audio_book_page/pages/search.dart';
 import 'package:flutter/material.dart';
 
-class AudiBook extends StatelessWidget {
+class AudiBook extends StatefulWidget {
   static const String routeName = "book";
-  PageController controller = PageController(viewportFraction: 0.6);
 
   AudiBook({super.key});
 
+  @override
+  State<AudiBook> createState() => _AudiBookState();
+}
+
+class _AudiBookState extends State<AudiBook> {
+  PageController controller = PageController(viewportFraction: 0.6);
+
   List<String> tabs = ["Art", "Business", "Comedy", "Drama"];
+
+int index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -29,11 +37,46 @@ class AudiBook extends StatelessWidget {
                       )),
                 ],
               ),
-              bottomNavigationBar:BottomNavBar(
-
-              ),
-              body: Home()
+              bottomNavigationBar:BottomNavigationBar(
+                  type: BottomNavigationBarType.fixed,
+                  currentIndex: index,
+                  onTap: (value) {
+                    index = value;
+                    setState(() {});
+                  },
+                  showUnselectedLabels: false,
+                  showSelectedLabels: false,
+                  unselectedItemColor: Colors.black,
+                  items: [
+                    BottomNavigationBarItem(
+                      icon: ImageIcon(
+                        AssetImage("assets/images/Icon.png"),
+                        size: 40,
+                      ),
+                      label: "",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: ImageIcon(
+                        AssetImage("assets/images/search.png"),
+                        size: 40,
+                      ),
+                      label: "",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: ImageIcon(
+                        AssetImage("assets/images/lib.png"),
+                        size: 40,
+                      ),
+                      label: "",
+                    ),
+                  ]),
+              body: page[index]
             ),));
   }
 
+List<Widget>page=[
+  Home(),
+  Search(),
+  Libaray()
+];
 }
